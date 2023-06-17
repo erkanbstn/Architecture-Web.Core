4329fbb26538126d4ea9393207572d0c994dea75
Erkan Bostan <profesyonel59@gmail.com>
2023-06-18 01:46:42 +0300

    Some Issues Fixed.

---
+++ LayersArchitecture.Core/ViewComponents/_ProjectPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayersArchitecture.Core.ViewComponents
+{
+    public class _ProjectPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
